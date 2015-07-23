6c614a318b9140cdd371c7fa4f124a3497025a34
defold94 <shooter_good@mail.ru>
2015-07-23 19:00:12 +0300

    lalala

--- lesson23_07.js
+++ lesson23_07.js
@@ -1,7 +1,13 @@
 /**
  * Created by defold on 23.07.2015.
+ * @return {number}
  */
 function RandomRange( min, max) {
-    return Matth.floor(Math.random() * (max - min) + min);
+    min = min || 1;
+    max = max || 10;
+
+    return Math.floor( Math.random () * ( max - min + 1 ) + min );
 }
-console.log(RandomRange);
+for(var i= 0; i<10; i++){
+    console.log(RandomRange(1,20));
+}
