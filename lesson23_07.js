5c3d8c690c8960817477c5848fd1985a74b190c0
defold94 <shooter_good@mail.ru>
2015-07-23 18:53:32 +0300

    Change RandomRange function

--- lesson23_07.js
+++ lesson23_07.js
@@ -1,7 +1,12 @@
 /**
  * Created by defold on 23.07.2015.
+ * @return {number}
  */
 function RandomRange( min, max) {
-    return Matth.floor(Math.random() * (max - min) + min);
+    min = min || 1;
+    max = max || 10;
+
+    return Math.floor( Math.random () * ( max - min ) + min );
 }
-console.log(RandomRange);
+console.log(RandomRange(1,20));
+console.log(RandomRange(1));
